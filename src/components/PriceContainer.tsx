1e2ff95354694e81754a9f92645ea23660c1eb78
yeasinmiah1272001 <137316520+yeasinmiah1272001@users.noreply.github.com>
2024-12-11 22:19:43 +0600

    price calculation complete

--- src/components/PriceContainer.tsx
+++ src/components/PriceContainer.tsx
@@ -7,22 +7,26 @@ const PriceContainer = ({ singleData }: { singleData: TreeBlogItem }) => {
   const [existingProduct, setExistingProduct] = useState<TreeBlogItem | null>(
     null
   );
+
   useEffect(() => {
     const availableProduct = cart.find((item) => item.id === singleData.id);
     setExistingProduct(availableProduct || null);
   }, [cart, singleData.id]);
-  const mainPrice = existingProduct
-    ? singleData.price * existingProduct?.quantity!
+
+  const mainPrice = existingProduct?.quantity
+    ? singleData.price * existingProduct.quantity
     : singleData.price;
-  const oldPrice = existingProduct
-    ? singleData.old_price * existingProduct?.quantity!
+
+  const oldPrice = existingProduct?.quantity
+    ? singleData.old_price * existingProduct.quantity
     : singleData.old_price;
+
   return (
     <div className="flex items-center gap-4">
       <p className="line-through text-xl font-medium text-gray-400">
         {oldPrice} $
       </p>
-      <p className=" text-xl font-medium">{mainPrice} $</p>
+      <p className="text-xl font-medium">{mainPrice} $</p>
     </div>
   );
 };
